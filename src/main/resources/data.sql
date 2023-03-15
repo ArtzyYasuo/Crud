0d3254f45975613d01a62267e6664665cc37d493
albirta yassine <albirtayassine70@outlook.fr>
2023-03-15 23:47:58 +0100

    add to resources

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into employees (first_name, last_name, salary) values('yassine', 'PyrrHa', '1200');
+insert into employees (first_name, last_name, salary) values('moad', 'benhomane', '1200');
+insert into employees (first_name, last_name, salary) values('ebdilah', 'lmghili', '1200');
